b76f6ee91df12c844cf23a2a9c3f327487d6c601
Punith B M <punith@designstring.com>
2024-03-05 10:59:04 +0530

    feat: initial commit

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+   <div>
+    <NounsBlockie :input="userAddress" />
+  </div>
+  </div>
+</template>
+
+<script>
+import { NounsBlockie } from 'vue-nouns-blockie';
+
+export default {
+  components: {
+    NounsBlockie
+  },
+  data() {
+    return {
+      userAddress: '0x...'
+    };
+  }
+}
+</script>
